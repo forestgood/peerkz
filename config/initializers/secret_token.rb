ca86ea2e5b2f47ad46af47a2e174e9018f942e92
Forest <fgoodiu@gmail.com>
2013-01-06 22:21:45 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peerkz::Application.config.secret_token = '953e6999039fb7697e7c6813409e84b4d6c486d1c13e1590d5d62e1e7ea6e967eff49fb5d6f04c7c23905251ac46d2bf65479e12fd68e7f18cd2a8ebd951ea4b'
